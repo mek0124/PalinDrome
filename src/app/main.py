f06bf6ec988b2d221246b3bfc6e4d3d146299af7
Mekasu <mekasurenae@gmail.com>
2025-05-09 23:52:05 -0500

    initial upload

---
+++ src/app/main.py
@@ -0,0 +1,44 @@
+from PySide6 import QtWidgets as qtw
+from src.app.main_window import Ui_MainWindow
+import sys
+
+
+class MainWindow(qtw.QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setupUi(self)
+        self.setWindowTitle("Palindrome Checker")
+        self.setMinimumWidth(800)
+        self.setMinimumHeight(600)
+
+        self.pb_Cancel.clicked.connect(self.close)
+        self.pb_Check.clicked.connect(self.check_palindrome)
+
+    def check_palindrome(self):
+        orig_word = self.le_WordToCheck.text()
+        backwards = orig_word[::-1]
+
+        self.is_error = True if backwards != orig_word else False
+
+        self.le_OriginalWord.setText(orig_word)
+        self.le_WordBackwards.setText(backwards)
+
+        self.le_OriginalWord.setEnabled(False)
+        self.le_WordBackwards.setEnabled(False)
+
+        if self.is_error:
+            self.le_ResultLabel.setText("False")
+            self.le_ResultLabel.setStyleSheet("color: red;")
+        else:
+            self.le_ResultLabel.setText("True")
+            self.le_ResultLabel.setStyleSheet("color: green;")
+
+
+if __name__ == '__main__':
+    app = qtw.QApplication(sys.argv)
+
+    window = MainWindow()
+    window.show()
+
+    sys.exit(app.exec())    
