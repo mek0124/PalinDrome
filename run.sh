677119c2a0b7ca18d06e69cb610e6ea38bab7100
Mekasu <mek0124@proton.me>
2025-05-21 16:56:05 -0500

    created run file

--- run.sh
+++ run.sh
@@ -1,30 +1,10 @@
 #!/bin/bash
 
-echo -e "Starting Run Process... Please Wait..."
+# Convert UI to Python
+pyside6-uic src/main_window.ui -o src/main_window.py
 
-sleep 2
+# Build the project
+python3 build.py
 
-echo -e "Obtaining Root Directory..."
-# get the root directory
-root_dir=$(pwd)
-
-echo -e "Root Dir: ${root_dir}"
-
-# check if the dist and build folders exist.
-# if not, build, else skip
-if [ ! -d "${root_dir}/dist" ] && [ ! -d "${root_dir}/build"]; then
-  # tell the user the folders don't exist and we're building them
-  echo -e "'dist' and 'build' folders do not exist. Building Project... Please Wait..."
-
-  # run the build command
-  if [ ! python3 build.py ]; then
-    echo -e "Build Failed. See output" >&2
-    exit 1
-  fi
-
-  echo -e "Build Complete..."
-fi
-
-echo -e "Running PalinDrome... Please Wait..."
-
-./dist/
+# Run the project
+./dist/PalinDrome/PalinDrome
