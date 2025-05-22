c52e20c1c5799b0a9b720335520702193ca684ab
Mekasu <mek0124@proton.me>
2025-05-22 15:26:00 -0500

    updated

--- run.sh
+++ run.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-
-# Convert UI to Python
-pyside6-uic src/main_window.ui -o src/main_window.py
-
-# Build the project
-python3 build.py
-
-# Run the project
-./dist/PalinDrome/PalinDrome
+# For Linux/Mac - Just runs the built executable
+./dist/PalinDrome
